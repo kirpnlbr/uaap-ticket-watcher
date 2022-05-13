20398518f4967f531357d17d871756d72c87d7de
Kirstine Peñalber <penalberkirstine@gmail.com>
2022-05-13 18:38:08 +0800

    create discord message function

--- src/main.py
+++ src/main.py
@@ -5,8 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-import website_checker
+from website_checker import change_value
 
 bot = discord.Client()
+
+@bot.event
+async def on_message(message):
+    if change_value is True:
+        await message.channel.send('Looks like an event has been added or removed from SM Tickets. Go check it out quick 👀 https://smtickets.com/events/category/sports')
 
 bot.run(DISCORD_TOKEN)
--- src/website_checker.py
+++ src/website_checker.py
@@ -1,39 +1,35 @@
 import time
 import hashlib
 from urllib.request import urlopen, Request
- 
+
 url = Request('https://smtickets.com/events/category/sports',
             headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'})
- 
-response = urlopen(url).read()
- 
-currentHash = hashlib.sha224(response).hexdigest()
-print("Checking for changes... Go do something else for awhile lol")
-time.sleep(10)
-while True:
-    try:
-        response = urlopen(url).read()
-        
-        currentHash = hashlib.sha224(response).hexdigest()
-        
-        time.sleep(15)
-        
-        response = urlopen(url).read()
-        
-        newHash = hashlib.sha224(response).hexdigest()
- 
-        if newHash == currentHash:
-            continue
- 
-        else:
-            print("Looks like an event has been added or removed from SM Tickets. Go check it out quick 👀")
- 
+
+def website_check(): 
+    change_value = website_check
+    
+    response = urlopen(url).read() 
+    currentHash = hashlib.sha224(response).hexdigest()
+    time.sleep(10)
+    while True:
+        try:
             response = urlopen(url).read()
- 
+                
             currentHash = hashlib.sha224(response).hexdigest()
- 
+                
             time.sleep(15)
-            continue
-            
-    except Exception as e:
-        print("Woops! An error has occurred 😅")
+                
+            response = urlopen(url).read()
+                
+            newHash = hashlib.sha224(response).hexdigest()
+        
+            if newHash == currentHash:
+                continue
+        
+            else:
+                response = urlopen(url).read()
+        
+                currentHash = hashlib.sha224(response).hexdigest()
+        
+                time.sleep(15)
+                continue
