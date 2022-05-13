617c26d8fb0660ae0297d9b7ad7f2377a6326d54
Kirstine Peñalber <penalberkirstine@gmail.com>
2022-05-13 18:44:05 +0800

    chore: change error var name

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 from website_checker import change_value
-from website_checker import error
+from website_checker import script_error
 
 bot = discord.Client()
 
@@ -15,7 +15,7 @@ async def on_message(message):
     if change_value is True:
         await message.channel.send('Looks like an event has been added or removed from SM Tickets. Go check it out quick 👀 https://smtickets.com/events/category/sports')
 
-    elif error is True:
+    elif script_error is True:
         await message.channel.send('An error has occurred woops. Fix me pls')
 
 bot.run(DISCORD_TOKEN)
